98fe1904eb032abd21253c623e61d7b6a6fb95d8
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2020-01-31 21:04:18 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,6 +69,8 @@ $("#formulario-de-contacto").submit(function(e) {
 	
 	var $form = $(this);
 	
+	console.log($form);
+	
 	$.post($form.attr("action"), $form.serialize()).then(function(data, status, xhr) {
  		alert("Obrigado pela tua mensagem! Entraremos em contacto assim que oportuno.");
 		console.log(data, status, xhr);
